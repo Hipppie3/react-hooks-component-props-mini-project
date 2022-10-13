87babc54d6b82f0b6af811e891b12a43edaa0da9
Daniel Hiep Truong <danieltruong89@yahoo.com>
2022-10-12 19:10:25 -0600

    commit tree 1e57aeeddc3f9eea4d9e21eab5086f6033cc096c

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function About ({image, about}) {
+ return (
+  <aside>
+   <img src={image} alt="Blog Logo"/>
+   <p>{about}</p>
+  </aside>
+ );
+
+}
+
+export default About;
